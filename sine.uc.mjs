d435d6f31f41f97c3b1a8b51932e6a6d539cab47
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2025-05-23 22:21:17 -0400

    Update sine.uc.mjs

--- sine.uc.mjs
+++ sine.uc.mjs
@@ -21,7 +21,7 @@ const Sine = {
     XUL: "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
     storeURL: "https://cosmocreeper.github.io/Sine/latest.json",
     scriptURL: "https://cosmocreeper.github.io/Sine/sine.uc.mjs",
-    updatedAt: "2025-05-22 17:00",
+    updatedAt: "2025-05-22 17:10",
     version: "1.1.5",
 
     async fetch(url, forceText=false) {
