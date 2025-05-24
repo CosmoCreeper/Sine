4d1012fc69848f787c3c176f3fc9120c9f263202
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2025-05-23 23:43:50 -0400

    Update sine.uc.mjs

--- sine.uc.mjs
+++ sine.uc.mjs
@@ -21,7 +21,7 @@ const Sine = {
     XUL: "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
     storeURL: "https://cosmocreeper.github.io/Sine/latest.json",
     scriptURL: "https://cosmocreeper.github.io/Sine/sine.uc.mjs",
-    updatedAt: "2025-05-23 23:01",
+    updatedAt: "2025-05-23 23:02",
     version: "1.1.6",
 
     async fetch(url, forceText=false) {
@@ -790,7 +790,7 @@ const Sine = {
             } else {
                 await setProperty("name", await githubAPI.name);
                 const releasesData = await localFetch(`${translateToAPI(repo)}/releases/latest`);
-                await setProperty("version", releasesData.tag_name.replace("v", "") || "1.0.0");
+                await setProperty("version", releasesData.hasOwnProperty("tag_name") ? releasesData.tag_name.replace("v", "") : "1.0.0");
             }
             await setProperty("description", githubAPI.description);
             await setProperty("createdAt", githubAPI.created_at);
