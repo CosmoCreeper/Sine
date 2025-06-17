e5578aaffae643fb757604dd5150d73eeeb102a6
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2025-06-16 22:37:21 -0400

    Add the C# compiler data.

---
+++ auto-installers/compiler.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+runtimes=("win-x64" "win-arm64" "osx-x64" "osx-arm64" "linux-x64" "linux-arm64")
+for runtime in "${runtimes[@]}"; do
+    dotnet publish -c Release -r $runtime --self-contained true -o "publish/$runtime"
+done
