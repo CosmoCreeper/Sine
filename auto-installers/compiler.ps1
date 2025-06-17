027eaebe7efa34c1985ffdc891417d30e7d019f7
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2025-06-17 08:09:05 -0400

    Update compiler.ps1

--- auto-installers/compiler.ps1
+++ auto-installers/compiler.ps1
@@ -1,4 +1,5 @@
 $runtimes = @("win-x64", "win-arm64", "osx-x64", "osx-arm64", "linux-x64", "linux-arm64")
 foreach ($runtime in $runtimes) {
-    dotnet publish -c Release -r $runtime --self-contained true -o "publish/$runtime"
+    Write-Host "Publishing for $runtime..."
+    dotnet publish -c Release -r $runtime --self-contained -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial -o "publish/$runtime"
 }
