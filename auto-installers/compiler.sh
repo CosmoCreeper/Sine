7cb3bdc3179b268c4ed881ceb530d67ff784ea8e
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2025-06-17 08:09:31 -0400

    Update compiler.sh

--- auto-installers/compiler.sh
+++ auto-installers/compiler.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 runtimes=("win-x64" "win-arm64" "osx-x64" "osx-arm64" "linux-x64" "linux-arm64")
 for runtime in "${runtimes[@]}"; do
-    dotnet publish -c Release -r $runtime --self-contained true -o "publish/$runtime"
+    echo "Publishing for $runtime..."
+    dotnet publish -c Release -r $runtime --self-contained -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial -o "publish/$runtime"
 done
