3eb216410e4697198eaa8dc6d70e4293d1110758
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-05-09 20:30:00 -0400

    Clarify max complexity comment in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,7 +40,7 @@ export default defineConfig([
         },
       ],
 
-      // Try to get to a maximum complexity of 20
+      // Use a smaller max complexity than Mozilla's default
       complexity: ["error", 20],
     },
   },
