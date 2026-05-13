046b77b673cb9c139b2c71325b5d6a7117c85727
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-05-12 23:16:18 -0400

    Switch from Biome to Oxc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 import mozilla from "eslint-plugin-mozilla";
 import noUnsanitizedPlugin from "eslint-plugin-no-unsanitized";
@@ -6,7 +5,7 @@ import noUnsanitizedPlugin from "eslint-plugin-no-unsanitized";
 export default defineConfig([
   ...mozilla.configs["flat/recommended"],
   {
-    ignores: [".browsercfg", "src/assets/imports/marked_parser.js"],
+    ignores: [".browsercfg", "src/assets/imports"],
   },
   {
     files: ["**/*.{js,mjs,cjs}"],
@@ -43,10 +42,5 @@ export default defineConfig([
       // Use a smaller max complexity than Mozilla's default
       complexity: ["error", 20],
     },
-  },
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/json",
   },
 ]);
